d043e630c015252cb88c83dc06f12aa458dc1632
triprioheryanto <triprioheryanto@gmail.com>
2025-12-13 00:22:22 +0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,21 +1,26 @@
 // firebase-init.js
 
-// Import modul yang dibutuhkan dari Firebase SDK (v12.6.1)
+// 1. Import modul yang dibutuhkan dari Firebase SDK (v12.6.1)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.1/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.1/firebase-database.js";
+// Import Cloud Firestore
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.1/firebase-firestore.js";
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDK0A-poPW1tOmi3d092fpabElu7JQgjKs", // API Key Anda 
-    authDomain: "database-imunisasi.firebaseapp.com", // 
-    projectId: "database-imunisasi", // 
-    storageBucket: "database-imunisasi.firebasestorage.app", // 
-    messagingSenderId: "897733986732", // 
-    appId: "1:897733986732:web:e2f402007e5f6678601209", // 
-    measurementId: "G-KFXGT89T5R" // 
+    // GANTI NILAI INI DENGAN KONFIGURASI PROYEK FIREBASE ANDA
+    apiKey: "AIzaSyDK0A-poPW1tOmi3d092fpabElu7JQgjKs", 
+    authDomain: "database-imunisasi.firebaseapp.com",
+    projectId: "database-imunisasi",
+    storageBucket: "database-imunisasi.firebasestorage.app",
+    messagingSenderId: "897733986732", 
+    appId: "1:897733986732:web:e2f402007e5f6678601209",
+    measurementId: "G-KFXGT89T5R" 
 };
 
 // Inisialisasi Firebase App
 const app = initializeApp(firebaseConfig);
 
-// Inisialisasi Realtime Database dan eksport objek 'db'
-export const db = getDatabase(app);
+// Inisialisasi Cloud Firestore dan eksport objek 'db' untuk digunakan di script.js
+export const db = getFirestore(app);
+
+// Catatan: Variabel 'db' sekarang adalah instance Cloud Firestore

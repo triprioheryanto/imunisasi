72dde147945cb0978670ab70f683cbf9da146451
triprioheryanto <triprioheryanto@gmail.com>
2026-02-12 21:37:59 +0700

    Switch from Firestore to Realtime Database

--- firebase-init.js
+++ firebase-init.js
@@ -1,26 +1,17 @@
 // firebase-init.js
-
-// 1. Import modul yang dibutuhkan dari Firebase SDK (v12.6.1)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.1/firebase-app.js";
-// Import Cloud Firestore
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.1/firebase-firestore.js";
-
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.1/firebase-database.js";
 
 const firebaseConfig = {
-    // GANTI NILAI INI DENGAN KONFIGURASI PROYEK FIREBASE ANDA
-    apiKey: "AIzaSyDK0A-poPW1tOmi3d092fpabElu7JQgjKs", 
+    apiKey: "AIzaSyDK0A-poPW1tOmi3d092fpabElu7JQgjKs",
     authDomain: "database-imunisasi.firebaseapp.com",
+    databaseURL: "https://database-imunisasi-default-rtdb.firebaseio.com/", // Sudah disesuaikan
     projectId: "database-imunisasi",
     storageBucket: "database-imunisasi.firebasestorage.app",
-    messagingSenderId: "897733986732", 
+    messagingSenderId: "897733986732",
     appId: "1:897733986732:web:e2f402007e5f6678601209",
-    measurementId: "G-KFXGT89T5R" 
+    measurementId: "G-KFXGT89T5R"
 };
 
-// Inisialisasi Firebase App
 const app = initializeApp(firebaseConfig);
-
-// Inisialisasi Cloud Firestore dan eksport objek 'db' untuk digunakan di script.js
-export const db = getFirestore(app);
-
-// Catatan: Variabel 'db' sekarang adalah instance Cloud Firestore
+export const db = getDatabase(app);

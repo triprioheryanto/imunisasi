812de870ae77d9d5b8dbda58a1601a33d57bbff2
triprioheryanto <triprioheryanto@gmail.com>
2026-02-12 21:59:15 +0700

    Refactor firebase initialization code

--- firebase-init.js
+++ firebase-init.js
@@ -1,11 +1,10 @@
-// firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.1/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.1/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDK0A-poPW1tOmi3d092fpabElu7JQgjKs",
     authDomain: "database-imunisasi.firebaseapp.com",
-    databaseURL: "https://database-imunisasi-default-rtdb.firebaseio.com/", // Sudah disesuaikan
+    databaseURL: "https://database-imunisasi-default-rtdb.firebaseio.com/",
     projectId: "database-imunisasi",
     storageBucket: "database-imunisasi.firebasestorage.app",
     messagingSenderId: "897733986732",
